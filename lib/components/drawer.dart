3215196d31615ceb8b724a56fc303847bf30c155
EllieNtasio <ellintasio@gmail.com>
2024-10-31 10:38:08 +0200

    first commit

---
+++ lib/components/drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color(0xFFC7D9B5),
+            ),
+            child:  Text(
+              'Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () {
+              // Δράση για το Home
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {
+              // Δράση για το Settings
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              // Δράση για το Logout
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
